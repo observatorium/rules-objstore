413b7b805b38c35a0f56aa8c7da834f7a19cf85e
Prem Saraswat <prmsrswt@gmail.com>
2021-12-14 19:01:51 +0530

    e2e: Test for multiple tenants

--- test/e2e/readwrite_test.go
+++ test/e2e/readwrite_test.go
@@ -19,13 +19,33 @@ import (
 	rulesspec "github.com/observatorium/api/rules"
 )
 
-var sampleRules = `
+var sampleRulesA = `
 groups:
   - name: test-oidc
     interval: 5s
     rules:
       - record: trs
-        expr: vector(1)`
+        expr: vector(1)
+      - alert: HighRequestLatency
+        expr: job:request_latency_seconds:mean5m{job="myjob"} > 0.5
+        for: 10m
+        labels:
+          severity: page
+        annotations:
+          summary: High request latency`
+
+var sampleRulesB = `
+groups:
+  - name: test-oidc
+    interval: 5s
+    rules:
+      - record: btrs
+        expr: vector(1)
+        labels:
+          dummy: yes
+      - alert: HighRequestLatency
+        expr: job:request_latency_seconds:mean5m{job="second"} > 0.5
+        for: 10m`
 
 var invalidRules = `
 groups:
@@ -61,6 +81,7 @@ func TestMetricsReadAndWrite(t *testing.T) {
 
 	ctx := context.Background()
 	tenantA := "tenant_a"
+	tenantB := "tenant_b"
 
 	t.Run("valid-rules-read-write", func(t *testing.T) {
 		rctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
@@ -69,7 +90,7 @@ func TestMetricsReadAndWrite(t *testing.T) {
 		// Retrying the first request as minio takes some time to get ready, even after readiness check passes.
 		// Details: https://github.com/efficientgo/e2e/issues/11.
 		err = runutil.Retry(time.Second*3, rctx.Done(), func() error {
-			res, err := client.SetRulesWithBody(ctx, tenantA, "application/yaml", strings.NewReader(sampleRules))
+			res, err := client.SetRulesWithBody(ctx, tenantA, "application/yaml", strings.NewReader(sampleRulesA))
 			if err != nil {
 				return err
 			}
@@ -82,14 +103,12 @@ func TestMetricsReadAndWrite(t *testing.T) {
 		})
 		testutil.Ok(t, err)
 
-		resp, err := client.ListRules(ctx, tenantA)
+		res, err := client.SetRulesWithBody(ctx, tenantB, "application/yaml", strings.NewReader(sampleRulesB))
 		testutil.Ok(t, err)
-		testutil.Equals(t, http.StatusOK, resp.StatusCode)
+		testutil.Equals(t, http.StatusOK, res.StatusCode)
 
-		respRules, err := ioutil.ReadAll(resp.Body)
-		testutil.Ok(t, err)
-
-		testutil.Equals(t, sampleRules, string(respRules))
+		checkRules(t, ctx, client, tenantA, sampleRulesA)
+		checkRules(t, ctx, client, tenantB, sampleRulesB)
 	})
 
 	t.Run("invalid-rules-read-write", func(t *testing.T) {
@@ -97,14 +116,18 @@ func TestMetricsReadAndWrite(t *testing.T) {
 		testutil.Equals(t, http.StatusBadRequest, res.StatusCode)
 		testutil.Ok(t, err)
 
-		resp, err := client.ListRules(ctx, tenantA)
-		testutil.Ok(t, err)
-		testutil.Equals(t, http.StatusOK, resp.StatusCode)
-
-		respRules, err := ioutil.ReadAll(resp.Body)
-		testutil.Ok(t, err)
-
 		// The rules retrieved should still match the prevoiusly set rules.
-		testutil.Equals(t, sampleRules, string(respRules))
+		checkRules(t, ctx, client, tenantA, sampleRulesA)
 	})
+}
+
+func checkRules(t *testing.T, ctx context.Context, client *rulesspec.Client, tenant, rules string) {
+	resp, err := client.ListRules(ctx, tenant)
+	testutil.Ok(t, err)
+	testutil.Equals(t, http.StatusOK, resp.StatusCode)
+
+	respRules, err := ioutil.ReadAll(resp.Body)
+	testutil.Ok(t, err)
+
+	testutil.Equals(t, rules, string(respRules))
 }
